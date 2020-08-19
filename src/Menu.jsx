3ea47a569bffeb81263614811aa76212c7c4d19f
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-08-18 20:20:33 -0500

    full

---
+++ src/Menu.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Search from './Search';
+import './Menu.css'
+import PanelAdd from './PanelAdd';
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {newItemPanel: false};
+        this.add = this.add.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+    }
+    add() { /* Aquí se podria utilizar las arrow functions para evitar los bind */
+        this.setState({newItemPanel: true});
+    }
+
+    onCancel() {
+        this.setState({newItemPanel: false});
+    }
+    render() {
+        return(
+            <div className="container">
+                <div className="subcontainer">
+                    <div className="logo">
+                        {this.props.title}
+                    </div>
+                    <div className="search">
+                        <Search onsearch={this.props.onsearch}/>
+                    </div>
+                    <div className="actions">
+                        <button onClick={this.add} className="button btn-blue">+ Añadir libro</button>
+                    </div>
+                </div>
+                    {
+                        (this.state.newItemPanel)? /* si es true */
+                            <PanelAdd oncancel={this.onCancel} onadd={this.props.onadd} />
+                        :
+                        ''
+                    }
+            </div>
+        )
+    }
+}
+export default Menu;
